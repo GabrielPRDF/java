09b48ead2c0e7c35562c8d93663e8bfb4114e11f
Gabriel Pinto Ribeiro da Fonseca <gabriel@Mac-mini-de-Gabriel.local>
2025-03-31 11:17:02 -0300

    Testes com print e variaveis

--- PrimeiroSoftware/src/Main.java
+++ PrimeiroSoftware/src/Main.java
@@ -1,9 +1,41 @@
+import java.util.Locale;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hellow word!");
+//		System.out.println("Hellow word!");
+//		System.out.println("Good morning!");
+//		int y = 30;
+//		double x = 10.35784;
+//		System.out.println(y);
+//		System.out.println(x);
+//		Locale.setDefault(Locale.US);	// Config local de execução do programa.
+//		System.out.printf("%.2f%n", x); // Config para definir 2 casas decimais depois do "." ou ",".
+//		System.out.printf("%.4f%n", x);	// Config para definir 4 casas decimais depois do "." ou ",".
+//		// Concatenações
+//		System.out.println("Resultado: " + x + " metros.");
+//		System.out.printf("Resultado: %.2f metros.%n", x);
+//		String nome = "Gabriel";
+//		int idade = 30;
+//		double valor = 13.000;
+//		System.out.printf("Concatenacao de %s e do %d e %.2f.%n", nome, idade, valor);
+		
+//		String product1 = "Computer";
+//		String product2 = "Office desk";
+//		
+//		int age = 30;
+//		int code = 5290;
+//		char gender = 'F';
+//		
+//		double price1 = 2100.0;
+//		double price2 = 650.50;
+//		double measure = 53.234567;
+//		
+//		System.out.println("Products:");
+//		System.out.printf("%s, which price is $ %.2f %n%s, which price is $ %.2f %n", product1, price1, product2, price2);
+//		System.out.printf("%nRecord: %d years old, code %d and gender: %c %n", age, code, gender);
+//		Locale.setDefault(Locale.US);
+//		System.out.printf("%nMeasue with eight decimal places: %.8f%nRouded (theree decimal places): %.3f%nUS decimal point: %.3f%n", measure, measure, measure);		
 	}
-
 }
