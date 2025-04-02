c6908db770a6c6ca5b4cf08c51dbd3f5bf8bed2d
Gabriel Pinto Ribeiro da Fonseca <gabriel@Mac-mini-de-Gabriel.local>
2025-04-02 19:45:56 -0300

    Finalizada lista 4

--- QuartoSoftware/quartoSoftware/src/application/Programa.java
+++ QuartoSoftware/quartoSoftware/src/application/Programa.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 import entities.Employee;
 import entities.Retangulo;
+import entities.Student;
 
 public class Programa {
 	
@@ -29,6 +30,15 @@ public class Programa {
 			sc.close();
 		}
 		
+		/*
+		 * Exercicio 2
+		 * Fazer um programa para ler os dados de um funcionário (nome, salário bruto e imposto).
+		 * Em seguida, mostrar os dados do funcionário (nome e salário líquido).
+		 * Em seguida, aumentar o salário do funcionário com base em uma porcentagem dada 
+		 * (somente o salário bruto é afetado pela porcentagem) e mostrar novamente os dados 
+		 * do funcionário. Use a classe projetada abaixo.
+		 */
+		
 		if (exercicio == 2) {
 			Scanner sc = new Scanner(System.in);
 			Employee person = new Employee();
@@ -45,7 +55,17 @@ public class Programa {
 		}
 		
 		if (exercicio == 3) {
-			
+			Scanner sc = new Scanner(System.in);
+			Student person = new Student();
+			System.out.println("Insert the name:");
+			person.nameAluno = sc.nextLine();
+			System.out.println("Insert score 1:");
+			person.tri1 = sc.nextDouble();
+			System.out.println("Insert score 2:");
+			person.tri2 = sc.nextDouble();
+			System.out.println("Insert score 3:");
+			person.tri3 = sc.nextDouble();
+			person.resultado();
 		}
 		
 	}
--- QuartoSoftware/quartoSoftware/src/entities/Student.java
+++ QuartoSoftware/quartoSoftware/src/entities/Student.java
@@ -1,5 +1,27 @@
 package entities;
 
 public class Student {
-
+	public String nameAluno;
+	public double tri1;
+	public double tri2;
+	public double tri3;
+	
+	private double media() {
+		return tri1 + tri2 + tri3;
+	}
+	
+	public void resultado() {
+		double resultadoMedia = media();
+		System.out.println("Media do aluno: " + resultadoMedia);
+		printResultado(resultadoMedia);
+	}
+	
+	private void printResultado(double resultadoMedia) {
+		if (resultadoMedia >= 60.0) {
+			System.out.println("Pass!");			
+		} else {
+			System.out.println("Failed!");
+			System.out.printf("Missing %.2f points!%n", 60.0 - resultadoMedia);
+		}
+	}
 }
