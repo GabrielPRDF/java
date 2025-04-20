572f5150a25d788794a009badd4c3c9d670cf3f3
Gabriel Fonseca <gabriel-prdf@hotmail.com>
2025-04-20 18:56:52 -0300

    Criando maven

--- SoftwareOneSpring/softOne/src/aplicacao/Programa.java
+++ SoftwareOneSpring/softOne/src/aplicacao/Programa.java
@@ -1,9 +1,19 @@
 package aplicacao;
 
+import dominio.Pessoa;
+
 public class Programa {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+
+		Pessoa p1 = new Pessoa(1, "Carlos da Silva", "carlos@gmail.com");
+		Pessoa p2 = new Pessoa(1, "Roberto da Silva", "roberto@gmail.com");
+		Pessoa p3 = new Pessoa(1, "Jose da Silva", "jose@gmail.com");
+		
+		System.out.println(p1);
+		System.out.println(p2);
+		System.out.println(p3);
 
 	}
 
