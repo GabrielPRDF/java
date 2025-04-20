0ff70721ebdedabab00ecc854f6f0c8a7cb4b9b6
Gabriel Fonseca <gabriel-prdf@hotmail.com>
2025-04-20 20:22:37 -0300

    Buscar dados com jpa/hibernate

--- SoftwareOneSpring/aulajpamaven/src/main/java/aplicacao/Programa.java
+++ SoftwareOneSpring/aulajpamaven/src/main/java/aplicacao/Programa.java
@@ -11,22 +11,28 @@ public class Programa {
 	public static void main(String[] args) {
 		
 
-		Pessoa p1 = new Pessoa(null, "Carlos da Silva", "carlos@gmail.com");
-		Pessoa p2 = new Pessoa(null, "Roberto da Silva", "roberto@gmail.com");
-		Pessoa p3 = new Pessoa(null, "Jose da Silva", "jose@gmail.com");
+//		Pessoa p1 = new Pessoa(null, "Carlos da Silva", "carlos@gmail.com");
+//		Pessoa p2 = new Pessoa(null, "Roberto da Silva", "roberto@gmail.com");
+//		Pessoa p3 = new Pessoa(null, "Jose da Silva", "jose@gmail.com");
 		
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
 		EntityManager em = emf.createEntityManager();
 		
-		em.getTransaction().begin();
+//		Para buscar dados!
+//		Pessoa p = em.find(Pessoa.class, 2);
+//		System.out.println(p);
 		
-		em.persist(p1);
-		em.persist(p2);
-		em.persist(p3);
-		
-		em.getTransaction().commit();
+//		Para persistir dados!
+//		em.getTransaction().begin();
+//		em.persist(p1);
+//		em.persist(p2);
+//		em.persist(p3);
+//		em.getTransaction().commit();
 		
 		System.out.println("End!!!");
+		
+		em.close();
+		emf.close();
 		
 //		System.out.println(p1);
 //		System.out.println(p2);
