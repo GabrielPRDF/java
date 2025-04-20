0e42ab5d316ba3d492679cbc1eacde6e7d0543e5
Gabriel Fonseca <gabriel-prdf@hotmail.com>
2025-04-20 20:18:25 -0300

    Subindo dados usando o jpa/hibernate

--- SoftwareOneSpring/aulajpamaven/src/main/java/aplicacao/Programa.java
+++ SoftwareOneSpring/aulajpamaven/src/main/java/aplicacao/Programa.java
@@ -1,5 +1,9 @@
 package aplicacao;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 import dominio.Pessoa;
 
 public class Programa {
@@ -7,13 +11,26 @@ public class Programa {
 	public static void main(String[] args) {
 		
 
-		Pessoa p1 = new Pessoa(1, "Carlos da Silva", "carlos@gmail.com");
-		Pessoa p2 = new Pessoa(1, "Roberto da Silva", "roberto@gmail.com");
-		Pessoa p3 = new Pessoa(1, "Jose da Silva", "jose@gmail.com");
+		Pessoa p1 = new Pessoa(null, "Carlos da Silva", "carlos@gmail.com");
+		Pessoa p2 = new Pessoa(null, "Roberto da Silva", "roberto@gmail.com");
+		Pessoa p3 = new Pessoa(null, "Jose da Silva", "jose@gmail.com");
 		
-		System.out.println(p1);
-		System.out.println(p2);
-		System.out.println(p3);
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+		EntityManager em = emf.createEntityManager();
+		
+		em.getTransaction().begin();
+		
+		em.persist(p1);
+		em.persist(p2);
+		em.persist(p3);
+		
+		em.getTransaction().commit();
+		
+		System.out.println("End!!!");
+		
+//		System.out.println(p1);
+//		System.out.println(p2);
+//		System.out.println(p3);
 
 	}
 
